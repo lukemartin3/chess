2089242d5853cacca826e56216173e5ba86d7fef
Luke Martin <lmartin9@uiowa.edu>
2023-10-06 03:25:49 -0500

    setup socket server

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ app.use(express.static('public'));
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
+// socket server setup
+var io = require('socket.io')(http);
+
+io.on('conection', function(socket) {
+    console.log('new connection');
+
+    socket.on('message', function(msg) {
+        console.log('Got message from client: ' + msg);
+    });
+});
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/default.html');
 });
