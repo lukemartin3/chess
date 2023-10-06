a558ab812d993be510a6bfa1242a14e8ae977706
Luke Martin <lmartin9@uiowa.edu>
2023-10-06 04:05:43 -0500

    server sends move to other clients

--- app.js
+++ app.js
@@ -3,17 +3,7 @@ var app = express();
 app.use(express.static('public'));
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
-
-// socket server setup
 var io = require('socket.io')(http);
-
-io.on('conection', function(socket) {
-    console.log('new connection');
-
-    socket.on('message', function(msg) {
-        console.log('Got message from client: ' + msg);
-    });
-});
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/default.html');
@@ -21,4 +11,12 @@ app.get('/', function(req, res) {
 
 http.listen(port, function() {
     console.log('listening on *: ' + port);
+});
+
+io.on('connection', function(socket) {
+    console.log('new connection');
+    
+    socket.on('move', function(msg) {
+       socket.broadcast.emit('move', msg); 
+    });
 });
--- public/default.js
+++ public/default.js
@@ -1,30 +1,28 @@
 var board;
 var game;
-
-window.onload = function() {
-    initGame();
-};
-
-// setup my socket client
 var socket = io();
 
-window.onClick = function(e){
-    socket.emit('message', 'hello Player');
-};
+window.onload = function () {initGame();};
 
 var initGame = function() {
-    var cfg = {
-        draggable: true,
-        position: 'start',
-        onDrop: handleMove,
-    };
-
-    board = new ChessBoard('gameBoard', cfg);
-    game = new Chess();
+   var cfg = {
+       draggable: true,
+       position: 'start',
+       onDrop: handleMove,
+   };
+   
+   board = new ChessBoard('gameBoard', cfg);
+   game = new Chess();
 };
 
-var handleMove = function(source, target) {
+var handleMove = function(source, target ) {
     var move = game.move({from: source, to: target});
-
-    if (move === null) return 'snapback';
+    
+    if (move === null)  return 'snapback';
+    else socket.emit('move', move);
 };
+
+socket.on('move', function(msg) {
+    game.move(msg);
+    board.position(game.fen());
+});
